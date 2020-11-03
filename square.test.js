53bb0ec46e1f5173877af3ba28494e1e6314f3d3
Blake Enloe <blakewenloe@yahoo.com>
2020-11-03 16:52:45 -0600

    converted test to arrow function for style consistency

--- square.test.js
+++ square.test.js
@@ -1,6 +1,6 @@
 const { square } = require("./square");
 
-test("should square a number", function () {
+test("should square a number", () => {
   const res = square(3);
   expect(res).toEqual(9);
 });
