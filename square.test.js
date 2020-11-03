34f1dfcd12cf1f453a6acd8ff9d05c686ed835a2
Blake Enloe <blakewenloe@yahoo.com>
2020-11-03 17:01:10 -0600

    added cube function tests

--- square.test.js
+++ square.test.js
@@ -1,4 +1,4 @@
-const { square } = require("./square");
+const { square, cube } = require("./square");
 
 describe("square functions", () => {
   test("should square a number", () => {
@@ -9,5 +9,17 @@ describe("square functions", () => {
   test("should square negative numbers", () => {
     const res = square(-3);
     expect(res).toEqual(9);
+  });
+});
+
+describe("cube functions", () => {
+  test("should cube a number", () => {
+    const res = cube(3);
+    expect(res).toEqual(27);
+  });
+
+  test("should cube negative numbers", () => {
+    const res = cube(-3);
+    expect(res).toEqual(-27);
   });
 });
